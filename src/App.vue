dca111d2923b37b0c372156d71f0362010e21fda
Faraz Hussain <fraz.hussain12@gmail.com>
2023-11-09 22:49:01 +0500

    Project Initialization

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Index from './components/Index.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <main id="hmn-main">
+    <header>
+      <div class="hmd-outer">
+        <div class="container">
+          <div class="hmd-inner">
+            <div class="hmd-logo">
+                <a href="index.html"> <img src="assets/images/Whie-Logo-scop3d.png" alt="Scop3D AI logo"
+                        style="width: 70%;"> </a>
+            </div>
+            <div class="hmd-navflx">
+                <div class="hmd-nav">
+                    <ul>
+                        <li class="hmd-navlist"><a href="#">Features</a></li>
+                        <li class="hmd-navlist"><a href="#">API</a></li>
+                    </ul>
+                </div>
+                <div class="upgrade-subtn">
+                    <a class="upgd-go" href="#">
+                        <img class="strlight" src="assets/images/light.svg" loading="lazy"
+                            alt="button light version upgrade homedesigns">
+                        Start Now
+                    </a>
+                </div>
+            </div>
+
+            <!-- <div class="mobileheader">
+              <div id="mySidenav" class="sidenav">
+                <div>
+                    <a href="javascript:void(0)" class="closebtn"
+                        onclick="if (!window.__cfRLUnblockHandlers) return false; closeNav()"
+                        data-cf-modified-fc83d25c74d8c30f99980e60->&times;</a>
+
+                    <a class="menu-link" href="login.html">Login</a>
+
+                    <a class="menu-link cls_menu" href="#generate">Start Now</a>
+                    <a class="menu-link" href="index.html#features">Features</a>
+                    <a class="menu-link" href="api.html">API</a>
+                    <a class="menu-link" href="go/blog/index.html">Blog</a>
+                    <a class="menu-link cls_menu" href="#buy">Upgrade to PRO</a>
+
+                </div>
+              </div>
+              <span class="only-mobile" style="font-size:40px; color: #7558ea;cursor:pointer"
+                  onclick="if (!window.__cfRLUnblockHandlers) return false; openNav()"
+                  data-cf-modified-fc83d25c74d8c30f99980e60->&#9776;</span>
+            </div> -->
+          </div>
+        </div>
+      </div>
+    </header>
+    <Index />
+    <Footer />
+  </main>
+        <!-- <RouterLink to="/">Home</RouterLink> -->
+    
+  <RouterView />
+</template>
+
+
---
+++ src/components/Footer.vue
@@ -0,0 +1,99 @@
+<template>
+    <footer>
+        <div class="ft-outer">
+            <div class="container">
+                <div class="bussiness-outer">
+                    <div class="competitive-inner">
+                        <div class="competitive-inner-left"> <img src="assets/images/bussiness-img.png"
+                                alt="real estate ai"> </div>
+                        <div class="competitive-inner-right">
+                            <h2 class="title-outer">Grow & Scale<br>
+                                <span>Your Business</span>
+                            </h2>
+                            <h3 class="sub-title">Get Started with Scop3D AI Today </h3>
+                            <p>Stand out in the competitive market with our AI-powered design tool. Enhance
+                                interiors, exteriors or gardens, visualize home transformations, and attract more
+                                potential customers. Show
+                                them not just what is, but what could be. Start today!</p>
+                            <a href="#" class="get-started">Start Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-main">
+                <div class="container">
+                    <div class="ft-top">
+                        <div class="row">
+                            <div class="col-lg-4 col-md-12">
+                                <div class="ft-bx">
+                                    <ul class="contact-list">
+                                        <li>
+                                            <a href="#">
+                                                <img src="assets/images/Whie-Logo-scop3d.png" loading="lazy"
+                                                    alt="Scop3D AI logo footer">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <p style="color:#fff;">"A picture is worth a thousand words, but a
+                                                perfect shot can sell a home." - Bryan Motta</p>
+                                        </li>
+    
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-2 col-md-3 col-sm-6">
+                                <div class="ft-bx">
+                                    <h6 class="footer-title">Connect with us</h6>
+                                    <ul class="social-list">
+                                        <li>
+                                            <a href="https://www.facebook.com">
+                                                <img src="assets/images/facebook.png" width="15px" loading="lazy"
+                                                    alt="facebook Scop3D AI">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.twitter.com/">
+                                                <img src="assets/images/twitter.png" width="15px" loading="lazy"
+                                                    alt="twitter homedesigns ai">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.youtube.com/">
+                                                <img src="assets/images/youtube.png" width="15px" loading="lazy"
+                                                    alt="youtube homedesigns ai">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.tiktok.com/">
+                                                <img src="assets/images/tiktok.png" width="15px" loading="lazy"
+                                                    alt="tiktok homedesigns ai">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.instagram.com/">
+                                                <img src="assets/images/instagram.png" width="15px" loading="lazy"
+                                                    alt="Scop3D AI instagram">
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://pinterest.com/">
+                                                <img src="assets/images/pinterest.png" width="15px" loading="lazy"
+                                                    alt="Scop3D AI pinterest">
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ft-btm">
+                        <p>Copyright 2023 Scop3D AI. all rights reserved</p>
+                    </div>
+                </div>
+                <span class="big-ai">AI</span>
+            </div>
+        </div>
+    </footer>
+</template>
+<script></script>
+
