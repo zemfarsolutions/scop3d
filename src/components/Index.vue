18940ac50b9974927795b9acbe84684b8b4eecc8
Faraz Hussain <fraz.hussain12@gmail.com>
2023-11-11 01:36:45 +0500

    Testimonial Slider

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -110,98 +110,24 @@
                 have used Scop3D to transform their<br> businesses, attract more clients, and sell properties
                 faster.</p>
             <div class="review-slider">
-                <div class="custom2 owl-carousel owl-theme">
-                    <div class="item">
-                        <div class="review-inner review-inner1">
-                            <div class="author-content">
-                                <p>"Scop3D has completely revolutionized the way I present properties. It allows me
-                                    to visualize the potential of each space and present it to my clients. My listings have
-                                    never looked better!" </p>
-                            </div>
-                            <div class="author-name">
-                                <h2>James M</h2>
-                                <p>Realtor, New York, USA </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="review-inner review-inner2">
-                            <div class="author-content">
-                                <p>"The virtual staging feature is a game-changer. I can now fill empty rooms with stylish
-                                    furniture and décor, creating a more engaging and attractive listing. My clients love
-                                    it!"</p>
-                            </div>
-                            <div class="author-name">
-                                <h2>Sarah T</h2>
-                                <p>Real Estate Agent, San Francisco, USA </p>
+                <Carousel :wrap-around="true" v-bind="settings" :wrapAround="true" :autoplay="3000" :breakpoints="breakpoints">
+                    <Slide v-for="slide in carouselSlides" :key="slide" class="custom2">
+                        <div class="item">
+                            <div class="review-inner review-inner1">
+                                <div class="author-content">
+                                    <p>"Scop3D has completely revolutionized the way I present properties. It allows me
+                                        to visualize the potential of each space and present it to my clients. My listings have
+                                        never looked better!" </p>
+                                </div>
+                                <div class="author-name">
+                                    <h2>James M</h2>
+                                    <p>Realtor, New York, USA </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="item">
-                        <div class="review-inner review-inner3">
-                            <div class="author-content">
-                                <p>"As a property manager, being able to transform outdated spaces into modern, appealing
-                                    interiors with just a few clicks has been invaluable. Scop3D has helped me sell
-                                    properties faster and at better prices." </p>
-                            </div>
-                            <div class="author-name">
-                                <h2>David R.,</h2>
-                                <p>Property Manager, Miami, USA </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="review-inner review-inner1">
-                            <div class="author-content">
-                                <p>"Scop3D has revolutionized my business. I can generate stunning interior visuals
-                                    in seconds, and this has saved me so much time and resources." </p>
-                            </div>
-                            <div class="author-name">
-                                <h2>Olivia P.</h2>
-                                <p>Real Estate Broker, Woodlands, Singapore </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="review-inner review-inner2">
-                            <div class="author-content">
-                                <p>"Scop3D is an essential tool for any real estate professional. It's helped me
-                                    attract more potential buyers and stand out in the competitive London property market."
-                                </p>
-                            </div>
-                            <div class="author-name">
-                                <h2>Benjamin K</h2>
-                                <p>Realtor, London, UK</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="review-inner review-inner3">
-                            <div class="author-content">
-                                <p>"By showcasing properties in various design styles, I've been able to appeal to a wider
-                                    range of potential buyers. Scop3D has been an invaluable tool in my work." </p>
-                            </div>
-                            <div class="author-name">
-                                <h2>Emilie C.,</h2>
-                                <p>Real Estate Agent, Monaco, France </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="review-inner review-inner3">
-                            <div class="author-content">
-                                <p>"Scop3D has been a game-changer for my business. It allows me to showcase
-                                    properties in their best light and give buyers a glimpse of what their future home could
-                                    look like." </p>
-                            </div>
-                            <div class="author-name">
-                                <h2>Richard L.,</h2>
-                                <p>Property Manager, Berlin, Germany
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    </Slide>
+
+                </Carousel>
             </div>
         </div>
     </section>
@@ -229,5 +155,47 @@
         </div>
     </section>
 </template>
+
+<script setup>
+import { Carousel, Slide } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
+
+const carouselSlides = ['Scop3D has completely revolutionized the way I present properties. It allows me\
+                            to visualize the potential of each space and present it to my clients. My listings have\
+                            never looked better!',
+                            'Scop3D has completely revolutionized the way I present properties. It allows me\
+                            to visualize the potential of each space and present it to my clients. My listings have\
+                            never looked better!',
+                            'Scop3D has completely revolutionized the way I present properties. It allows me\
+                            to visualize the potential of each space and present it to my clients. My listings have\
+                            never looked better!'];
+
+const settings  = {
+    itemsToShow: 3,
+    snapAlign: 'center',
+}
+
+const breakpoints = {
+      // 700px and up
+      0: {
+        itemsToShow: 1,
+        snapAlign: 'center',
+        nav: false
+      },
+      //
+      600: {
+        itemsToShow: 2,
+        snapAlign: 'start',
+        nav: false
+      },
+      // 1024 and up
+      1022: {
+        itemsToShow: 3,
+        snapAlign: 'start',
+        nav: false
+      }
+}
+</script>
+
 
 
