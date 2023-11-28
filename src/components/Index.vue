db84896521a45531e1b9ba7fe91c3271b7a9014d
developerzemfar <developerzemfar@gmail.com>
2023-11-28 19:58:07 +0500

    Still working

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -69,7 +69,7 @@
                 enhancing its value and appeal.</p>
             <!-- <img src="/assets/images/real-estate-text-color-change.jpg" alt=""> -->
             <div id="uploader_section" class="uploader_section">
-                <form class="w-full mx-auto p-4 lg:p-8 bg-brand_500 bg-opacity-20 rounded-3xl space-y-4" >
+                <form class="w-full mx-auto p-4 lg:p-8 bg-brand_500 bg-opacity-20 rounded-3xl space-y-4">
                     <div>
                         <div class="file-pond-dashboard rounded-lg dashed-border relative cursor-pointer overflow-hidden"
                             id="browse-HP">
@@ -81,29 +81,31 @@
                                         data-style-load-indicator-position="right"
                                         data-style-progress-indicator-position="right"
                                         data-style-button-remove-item-align="false" style="height: 446px;">
-                                        <input class="filepond--browser filepond" type="file" id="filepond--browser-2otqr6ayx"
-                                            name="filepond" aria-controls="filepond--assistant-2otqr6ayx" @change="submit" @input="updateFile">
+                                        <input class="filepond--browser filepond" type="file"
+                                            id="filepond--browser-2otqr6ayx" name="filepond"
+                                            aria-controls="filepond--assistant-2otqr6ayx" @change="submit"
+                                            @input="updateFile">
                                         <div class="filepond--drop-label"
                                             style="transform: translate3d(0px, 0px, 0px); opacity: 1;">
                                             <label for="filepond--browser-2otqr6ayx" id="filepond--drop-label-2otqr6ayx"
                                                 aria-hidden="true">
-                                                <div class="flex flex-col items-center lg:max-w-[245px] !cursor-pointer  ">
-                                                    <img src="/assets/images/uploader-icon.svg" width="64" class="mb-4">
-                                                    <span
-                                                        class="text-title-sm text-brand_200 mb-4 font-bold flex  tracking-wide "><span
-                                                            class="lg:flex hidden mr-1">Drag or</span> Select Your
-                                                        Photo</span>
+                                                <div class="flex flex-col items-center lg:max-w-[100%] !cursor-pointer  ">
+                                                    <img src="/assets/images/uploader-icon.svg" width="50%" class="mb-4">
+                                                    <file-pond name="test" ref="pond" label-idle="Drop files here..."
+                                                        v-bind:allow-multiple="true"
+                                                        accepted-file-types="image/jpeg, image/png" v-bind:files="myFiles"
+                                                        v-bind:server="MyServer" @addfile="onAddFile" />
                                                     <span
                                                         class="  text-body-sm leading-5 tracking-wide text-brand_300 mb-4 ">
                                                         Supported
                                                         formats: JPG, JPEG, PNG, WEBP
                                                         Max size 50 MB
                                                     </span>
-                                                    <div
+                                                    <!-- <div
                                                         class="border flex justify-center items-center !rounded-full bg-brand_500  text-gray_100 border-none outline-none py-2 pr-6 ps-4 mb-2">
                                                         <img src="/assets/images/upload-icon.svg">
                                                         <span>Upload</span>
-                                                    </div>
+                                                    </div> -->
                                                 </div>
                                             </label>
                                         </div>
@@ -129,59 +131,79 @@
                         &nbsp; &nbsp; Try For FREE </div> -->
                         </div>
                         <!-- <div class="lg:h-[500px] h-[240px] w-full white bg-gray_700 rounded-lg animate-pulse" style="display: none;">
-        </div> -->
+                            </div> -->
                     </div>
 
                     <div class="flex items-center justify-between lg:space-x-4 space-x-2 overflow-x-auto pb-1">
-        <div><a
-            class="flex flex-col rounded-2xl relative text-center px-4 lg:py-4 py-2 cursor-pointer transition-all hover:bg-opacity-100 bg-[#0000001A] min-w-[165px] opacity-40 pointer-events-auto !cursor-default"
-            id="HP-IDservice-selection">
-            <div class="mb-2"><img src="https://revi.rvstatic.com/aihomedesign/header-icons/inactive-ID.svg"
-                loading="lazy" width="24" class="mx-auto"></div>
-            <p class="text-body-md text-brand_300">Interior Design</p>
-          </a></div>
-        <div><a
-            class="flex flex-col rounded-2xl relative text-center px-4 lg:py-4 py-2 cursor-pointer transition-all hover:bg-opacity-100 bg-[#0000001A] min-w-[165px] opacity-40 pointer-events-auto !cursor-default"
-            id="HP-VSservice-selection">
-            <div class="mb-2"><img src="https://revi.rvstatic.com/aihomedesign/header-icons/inactive-VS.svg"
-                loading="lazy" width="24" class="mx-auto"></div>
-            <p class="text-body-md text-brand_300">Virtual Staging</p>
-          </a></div>
-        <div><a
-            class="flex flex-col rounded-2xl relative text-center px-4 lg:py-4 py-2 cursor-pointer transition-all hover:bg-opacity-100 bg-[#0000001A] min-w-[165px] opacity-40 pointer-events-auto !cursor-default"
-            id="HP-IEservice-selection">
-            <div class="mb-2"><img src="https://revi.rvstatic.com/aihomedesign/header-icons/inactive-IE.svg"
-                loading="lazy" width="24" class="mx-auto"></div>
-            <p class="text-body-md text-brand_300">Image Enhancement</p>
-          </a></div>
-        <div><a
-            class="flex flex-col rounded-2xl relative text-center px-4 lg:py-4 py-2 cursor-pointer transition-all hover:bg-opacity-100 bg-[#0000001A] min-w-[165px] opacity-40 pointer-events-auto !cursor-default"
-            id="HP-DDservice-selection">
-            <div class="mb-2"><img src="https://revi.rvstatic.com/aihomedesign/header-icons/inactive-D2D.svg"
-                loading="lazy" width="24" class="mx-auto"></div>
-            <p class="text-body-md text-brand_300">Day to Dusk</p>
-          </a></div>
-        <div><a
-            class="flex flex-col rounded-2xl relative text-center px-4 lg:py-4 py-2 cursor-pointer transition-all hover:bg-opacity-100 bg-[#0000001A] min-w-[165px] opacity-40 pointer-events-auto !cursor-default"
-            id="HP-IRservice-selection">
-            <div class="mb-2"><img src="https://revi.rvstatic.com/aihomedesign/header-icons/inactive-IR.svg"
-                loading="lazy" width="24" class="mx-auto"></div>
-            <p class="text-body-md text-brand_300">Item Removal</p>
-          </a></div>
-      </div>
+                        <div><a class="flex flex-col rounded-2xl relative text-center px-4 lg:py-4 py-2 cursor-pointer transition-all hover:bg-opacity-100 bg-[#0000001A] min-w-[165px] opacity-40 pointer-events-auto !cursor-default"
+                                id="HP-IDservice-selection">
+                                <div class="mb-2"><img
+                                        src="https://revi.rvstatic.com/aihomedesign/header-icons/inactive-ID.svg"
+                                        loading="lazy" width="24" class="mx-auto"></div>
+                                <p class="text-body-md text-brand_300">Interior Design</p>
+                            </a></div>
+                        <div>
+                            <a class="flex flex-col rounded-2xl relative text-center px-4 lg:py-4 py-2 cursor-pointer transition-all hover:bg-opacity-100 bg-[#0000001A] min-w-[165px] opacity-40 pointer-events-auto !cursor-default"
+                                id="HP-VSservice-selection">
+                                <div class="mb-2"><img
+                                        src="https://revi.rvstatic.com/aihomedesign/header-icons/inactive-VS.svg"
+                                        loading="lazy" width="24" class="mx-auto"></div>
+                                <p class="text-body-md text-brand_300">Virtual Staging</p>
+                            </a>
+                        </div>
+                        <div><a class="flex flex-col rounded-2xl relative text-center px-4 lg:py-4 py-2 cursor-pointer transition-all hover:bg-opacity-100 bg-[#0000001A] min-w-[165px] opacity-40 pointer-events-auto !cursor-default"
+                                id="HP-IEservice-selection">
+                                <div class="mb-2"><img
+                                        src="https://revi.rvstatic.com/aihomedesign/header-icons/inactive-IE.svg"
+                                        loading="lazy" width="24" class="mx-auto"></div>
+                                <p class="text-body-md text-brand_300">Image Enhancement</p>
+                            </a></div>
+                        <div><a class="flex flex-col rounded-2xl relative text-center px-4 lg:py-4 py-2 cursor-pointer transition-all hover:bg-opacity-100 bg-[#0000001A] min-w-[165px] opacity-40 pointer-events-auto !cursor-default"
+                                id="HP-DDservice-selection">
+                                <div class="mb-2"><img
+                                        src="https://revi.rvstatic.com/aihomedesign/header-icons/inactive-D2D.svg"
+                                        loading="lazy" width="24" class="mx-auto"></div>
+                                <p class="text-body-md text-brand_300">Day to Dusk</p>
+                            </a></div>
+                        <div><a class="flex flex-col rounded-2xl relative text-center px-4 lg:py-4 py-2 cursor-pointer transition-all hover:bg-opacity-100 bg-[#0000001A] min-w-[165px] opacity-40 pointer-events-auto !cursor-default"
+                                id="HP-IRservice-selection">
+                                <div class="mb-2"><img
+                                        src="https://revi.rvstatic.com/aihomedesign/header-icons/inactive-IR.svg"
+                                        loading="lazy" width="24" class="mx-auto"></div>
+                                <p class="text-body-md text-brand_300">Item Removal</p>
+                            </a></div>
+                    </div>
                 </form>
             </div>
         </div>
-        <file-pond
-            name="test"
-            ref="pond"
-            label-idle="Drop files here..."
-            v-bind:allow-multiple="true"
-            accepted-file-types="image/jpeg, image/png"
-            v-bind:server="apiEndpoint"
-            :files="myFiles"
-            @change="handleFilePondInit"
-        />
+
+        <div class="container">
+            <div>
+                <file-pond name="test" ref="pond" label-idle="Drop files here..." v-bind:allow-multiple="true"
+                    accepted-file-types="image/jpeg, image/png" v-bind:files="myFiles" v-bind:server="MyServer"
+                    @addfile="onAddFile" />
+            </div>
+            <div class="row d-flex">
+                <div v-for="(image, index) in images" :key="index" class="col-md-3">
+                    <div class="custom-control custom-checkbox image-checkbox">
+                        <input type="radio" @change="selectedImage = image.id" class="form-check-input" style="position: absolute; z-index: 9999;background-color: #198754;
+                                                                                            border-color: #198754;"
+                            name="selectImg">
+                        <label class="custom-control-label" for="ck1a">
+                            <img :src="image.src" alt="#" class="img-fluid" style="width: 200px;position: relative;">
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div v-if="images.length > 0">
+                <button class="btn btn-sm btn-success" @click="generate" style="font-size: 15px">Generate AI</button>
+            </div>
+
+            <!-- SINGLE SELECTION -->
+            <!-- <vue-select-image :dataImages="images" @onselectimage="onSelectImage">
+            </vue-select-image> -->
+        </div>
+
     </section>
 
     <section class="streamline-outer">
@@ -302,9 +324,12 @@ import axios from 'axios'
 import { reactive, ref } from 'vue';
 import FormData from 'form-data';
 
-
 const fileInput = ref(null);
+const selectedImage = ref();
 
+function generate() {
+    console.log(selectedImage.value)
+}
 const updateFile = (event) => {
     fileInput.value = event.target.files[0];
 };
@@ -360,7 +385,7 @@ const breakpoints = {
 </script>
 
 
-<script>
+<!-- <script>
 import axios from 'axios';
 // Import Vue FilePond
 import vueFilePond from "vue-filepond";
@@ -399,28 +424,30 @@ export default {
       // FilePond instance methods are available on `this.$refs.pond`
     },
     async submit() {
-      const formData = new FormData();
-      const fileInput = this.$refs.pond.getFiles(); // Get the first file
-console.log(fileInput);
-      formData.append('image', fileInput);
-      formData.append('serviceName', 'VS');
-
-      if (fileInput && fileInput.file) {
         const formData = new FormData();
-        formData.append('image', fileInput.file);
+        const fileInput = this.$refs.pond.getFiles(); // Get the first file
+
+        console.log(fileInput);
+
+        formData.append('image', fileInput);
         formData.append('serviceName', 'VS');
 
-        try {
-        this.apiEndpoint = 'https://api.aihomedesign.com/api/public/v1/ai/image/upload?image'
+        if (fileInput && fileInput.file) {
+            const formData = new FormData();
+            formData.append('image', fileInput.file);
+            formData.append('serviceName', 'VS');
 
-          const response = await axios.post('https://api.aihomedesign.com/api/public/v1/ai/image/upload?image', formData);
-          console.log(response);
-        } catch (error) {
-          console.error(error);
+            try {
+            this.apiEndpoint = 'https://api.aihomedesign.com/api/public/v1/ai/image/upload?image'
+
+            const response = await axios.post('https://api.aihomedesign.com/api/public/v1/ai/image/upload?image', formData);
+            console.log(response);
+            } catch (error) {
+            console.error(error);
+            }
+        } else {
+            console.error('No file selected');
         }
-      } else {
-        console.error('No file selected');
-      }
 
     },
   
@@ -428,5 +455,63 @@ console.log(fileInput);
   components: {
     FilePond,
   },
+};
+</script> -->
+
+<script>
+// Import Vue FilePond
+import vueFilePond from "vue-filepond";
+
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+
+// Import FilePond plugins
+// Please note that you need to install these plugins separately
+
+// Import image preview plugin styles
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+
+// Import image preview and file type validation plugins
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+
+// Create component
+const FilePond = vueFilePond(
+    FilePondPluginFileValidateType,
+    FilePondPluginImagePreview
+);
+
+export default {
+    name: "app",
+    data: function () {
+        return {
+            myFiles: [],
+            images: [],
+            MyServer: {
+                url: 'https://api.aihomedesign.com/api/public',
+                timeout: 7000,
+                process: {
+                    url: '/v1/ai/image/upload',
+                    method: 'POST',
+                    withCredentials: false,
+                    onload: (response) => {
+                        const obj = JSON.parse(response)
+                        this.images.push(obj)
+
+                        console.log()
+                    },
+                    onerror: (response) => console.log(response),
+                    ondata: (formData) => {
+                        formData.append('image', this.$refs.pond.getFiles()[0].file);
+                        formData.append('serviceName', 'VS');
+                        return formData;
+                    },
+                },
+            }
+        };
+    },
+    components: {
+        FilePond,
+    },
 };
 </script>
